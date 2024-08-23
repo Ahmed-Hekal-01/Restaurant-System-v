d1222e377bc79556630322d1b20ab6cca772973c
Omar <omarelwkel17@gmail.com>
2024-08-23 20:38:03 +0300

    Edited food class

--- Restaurant-System/Shared data.cpp
+++ Restaurant-System/Shared data.cpp
@@ -22,3 +22,38 @@
 #include<cctype>
 using namespace std;
 
+class Shared_Data
+{
+private:
+	string name;
+	double price;
+	int quantity;
+public:
+	void Set_name(string name)
+	{
+		this->name = name;
+	}
+	void Set_price(double price)
+	{
+		this->price = price;
+
+	}
+	void Set_Quantity(int quantity)
+	{
+		this->quantity = quantity;
+	}
+
+	string Get_name()
+	{
+		return name;
+	}
+	double Get_price()
+	{
+		return price;
+	}
+	int Get_quantity()
+	{
+		return quantity;
+	}
+
+};
