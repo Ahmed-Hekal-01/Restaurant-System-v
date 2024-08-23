8e5f7bbe973eb72d0535d4804822800722514d44
Omar <omarelwkel17@gmail.com>
2024-08-23 20:26:38 +0300

    Edited food class

--- Restaurant-System/Food.cpp
+++ Restaurant-System/Food.cpp
@@ -1,5 +1,5 @@
 #include "Shared data.cpp"
-class food : public shared_Data {
+class food : public Shared_Data {
 private:
 	vector<food>normal_food, sea_food;
 	vector<food>food_by_price;
@@ -7,26 +7,26 @@ public:
 	void set_normal_food(food food) {
 		normal_food.push_back(food);
 	}
-	void set_sea_food() {
+	void set_sea_food(food food) {
 		sea_food.push_back(food);
 	}
 	void update_normal_name(string name, int index) {
-		normal_food[index].set_name(name);
+		normal_food[index].Set_name(name);
 	}
 	void update_sea_name(string name, int index) {
-		sea_food[index].set_name(name);
+		sea_food[index].Set_name(name);
 	}
 	void update_normal_price(int price, int index) {
-		normal_food[index].set_price(price);
+		normal_food[index].Set_price(price);
 	}
 	void update_sea_price(int price, int index) {
-		sea_food[index].set_price(price);
+		sea_food[index].Set_price(price);
 	}
-	void remove_normal_food(string food) {
-		normal_food.erase(normal_food.begin() + normal_food[food]);
+	void remove_normal_food(int index) {
+		normal_food.erase(normal_food.begin() + index);
 	}
-	void remove_sea_food(string food) {
-		sea_food.erase(sea_food.begin() + sea_food[food]);
+	void remove_sea_food(int index) {
+		sea_food.erase(sea_food.begin() + index);
 	}
 	void Update_normal_food_Quantity(int quantity, int p) {
 		normal_food[p].Set_Quantity(quantity);
@@ -34,11 +34,21 @@ public:
 	void Update_sea_food_Quantity(int quantity, int p) {
 		sea_food[p].Set_Quantity(quantity);
 	}
-	void search_normal_food(string food) {
-		return normal_food[food];
+	int search_normal_food(string food) {
+		for (int i = 0; i < normal_food.size(); i++) {
+			if (normal_food[i].Get_name() == food) {
+				return i;
+			}
+			else return -1;
+		}
 	}
-	void search_sea_food(string food) {
-		return sea_food[food];
+	int search_sea_food(string food) {
+		for (int i = 0; i < sea_food.size(); i++) {
+			if (sea_food[i].Get_name() == food) {
+				return i;
+			}
+			else return -1;
+		}
 	}
 	void display_normal_food() {
 		for (auto i : normal_food) {
