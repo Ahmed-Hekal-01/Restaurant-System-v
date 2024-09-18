2988027231d05bdf374ba7362276becb890ab774
Mostafa200425 <109903163+Mostafa200425@users.noreply.github.com>
2024-09-18 06:58:40 +0300

    users file added

---
+++ Restaurant-System/registeration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class User {
+private:
+    string username;
+    string password;
+    string id;
+};
+
+void Register_New_User(string username, string password) {
+
+}
